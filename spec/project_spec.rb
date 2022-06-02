59224ffbea85148a3bdc914d93acc9025dfb03db
Anthony Gladden <a.gladden360@outlook.com>
2022-06-01 17:09:03 -0700

    added spec_helper.rb

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -9,4 +9,16 @@ describe Project do
   end
 end
 
+  context '#id' do
+  it 'returns id before saving' do
+    project = Project.new({:title => 'humpty dumpty',:id => nil})
+    expect(project.id).to(eq(nil))
+  end
 end
+  it 'returns id after saving' do
+    project = Project.new({:title => 'humpty dumpty',:id => nil})
+    project.save
+    expect(project.id).to(be_an_instance_of(Integer))
+  end
+end
+
